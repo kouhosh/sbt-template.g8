8e845dae1a7ae7bbf2b12f3ab1cd1b1b5c96cb33
Kohei Hoshino <kou.hosh@gmail.com>
2017-07-12 22:59:28 +0900

    テストサンプルの更新

--- src/main/g8/src/test/scala/ExampleSpec.scala
+++ src/main/g8/src/test/scala/ExampleSpec.scala
@@ -1,20 +1,30 @@
-import collection.mutable.Stack
 import org.scalatest._
 
 class ExampleSpec extends FlatSpec with Matchers {
+  trait Stack[A] {
+    def push(a: A): (Unit, Stack[A])
+    def pop: (A, Stack[A])
+  }
+
+  case class ListStack[A](l: List[A]) extends Stack[A] {
+    def push(a: A): (Unit, Stack[A]) = ((), ListStack(a :: l))
+    def pop: (A, Stack[A]) = (l.head, ListStack(l.tail))
+  }
 
   "A Stack" should "pop values in last-in-first-out order" in {
-    val stack = new Stack[Int]
-    stack.push(1)
-    stack.push(2)
-    stack.pop() should be (2)
-    stack.pop() should be (1)
+    val s = ListStack(List.empty[Int])
+    val (_, s1) = s.push(1)
+    val (_, s2) = s1.push(2)
+    val (a1, s3) =  s2.pop
+    val (a2, _) = s3.pop
+    a1 should be (2)
+    a2 should be (1)
   }
 
   it should "throw NoSuchElementException if an empty stack is popped" in {
-    val emptyStack = new Stack[Int]
+    val emptyStack = ListStack(List.empty[Int])
     a [NoSuchElementException] should be thrownBy {
-      emptyStack.pop()
+      emptyStack.pop
     }
   }
 }
